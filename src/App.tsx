cf82b9a89eee55c6f9ef050efa7f3aa8c80646d0
Rakesh Choudhary <seervir987@gmail.com>
2025-03-25 09:04:25 +0530

    Add curson

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { CarouselDemo } from "./components/CarouselDemo";
+import Cursor from "./components/Curson";
 import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
     <div className="w- h-full">
+      <Cursor />
       <HeroSection />
       <CarouselDemo />
     </div>
