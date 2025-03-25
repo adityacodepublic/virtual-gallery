5be25a5ce17502a641d93055ac82b391cc33b5a6
gitcode <website0wordpress@gmail.com>
2025-03-25 09:43:21 +0530

    push splin e

--- src/Spline.tsx
+++ src/Spline.tsx
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function Spliner() {
   return (
-    <main>
+    <main className="w-screen h-screen">
       <Spline scene="https://prod.spline.design/375Ev6WouaPTJhwS/scene.splinecode" />
     </main>
   );
