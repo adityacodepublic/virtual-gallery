14c52d9c45bbb5c33963a3ec7f3d5bedf40bcb9c
gitcode <website0wordpress@gmail.com>
2025-07-05 10:55:46 +0530

    globe styles

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,9 +1,12 @@
 import Spline from "@splinetool/react-spline";
+import { useEffect } from "react";
 
 export default function Globe() {
+  useEffect(() => {}, []);
   return (
     <main className="w-screen h-screen">
       <Spline scene="https://prod.spline.design/375Ev6WouaPTJhwS/scene.splinecode" />
+      <div className="fixed bottom-0 right-0 w-[160px] h-[70px] bg-[#bbddfd] z-50"></div>
     </main>
   );
 }
