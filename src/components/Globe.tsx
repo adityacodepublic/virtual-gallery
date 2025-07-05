7f26cb8f65e0b27af4afebaf02b2a64adf193dfc
gitcode <website0wordpress@gmail.com>
2025-07-05 11:02:48 +0530

    bg styles

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -12,7 +12,7 @@ export default function Globe() {
     return null;
   }
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen bg-[#bbddfd]">
       <Spline scene="https://prod.spline.design/375Ev6WouaPTJhwS/scene.splinecode" />
       <div className="fixed bottom-0 right-0 w-[160px] h-[70px] bg-[#bbddfd] z-50"></div>
     </main>
